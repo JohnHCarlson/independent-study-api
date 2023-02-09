1123c7f9b6acb76fcf6ebabfe58d76b008174022
John Carlson <JCJohnCarlson@gmail.com>
2023-02-09 11:29:36 -0500

    Added categorization model

---
+++ Models/Categorization.cs
@@ -0,0 +1,15 @@
+﻿namespace PindexBackend.Models {
+
+    public class Categorization {
+
+        //PK
+        public int CategorizationId { get; set; }
+
+        //Per-categorization data
+        public string Name { get; set; }
+
+        //Many-to-many data
+        public ICollection<Item> Items;
+    }
+}
+
