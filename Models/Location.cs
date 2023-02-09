a6e2cc1b9b9f214e8c94b2b4645b24480c4928ba
John Carlson <JCJohnCarlson@gmail.com>
2023-02-09 11:30:13 -0500

    Added location  model

---
+++ Models/Location.cs
@@ -0,0 +1,16 @@
+﻿namespace PindexBackend.Models {
+
+    public class Location {
+
+        //PK
+        public int LocationId { get; set; }
+
+        //Per-location data
+        public required string Name { get; set; }
+
+        //Many-to-one data
+        public int ItemId { get; set; }
+        public Item? Item { get; set; }
+    }
+}
+
