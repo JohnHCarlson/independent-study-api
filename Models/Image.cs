df91c8003685c95c1bafa1e0b61d22fcbace8f9d
John Carlson <jcjohncarlson@gmail.com>
2023-02-12 21:20:21 -0500

    Created image model

---
+++ Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PindexBackend.Models {
+    public class Image {
+
+        //PK
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ImageId { get; set; }
+        
+        //Per-image data
+        public DateTime UploadDate { get; set; }
+        public required string FileName { get; set; }
+        
+        //One-to-one data
+        public int ItemId { get; set; }
+        public Item? Item { get; set; }
+    }
+}
