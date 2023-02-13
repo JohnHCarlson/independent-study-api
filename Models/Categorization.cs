2c40b2f9e0c405b8b242e2ef2ef9d3c764815790
John Carlson <jcjohncarlson@gmail.com>
2023-02-12 21:19:37 -0500

    Updated name value to be required

--- Models/Categorization.cs
+++ Models/Categorization.cs
@@ -10,10 +10,10 @@ namespace PindexBackend.Models {
         public int CategorizationId { get; set; }
 
         //Per-categorization data
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         //Many-to-many data
-        public ICollection<Item> Items;
+        public ICollection<Item>? Items { get; set; }
     }
 }
 
