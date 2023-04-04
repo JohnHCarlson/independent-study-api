327c723cd9de27c8f1e029705b12ccbe2d9fac57
John Carlson <jcjohncarlson@gmail.com>
2023-04-03 20:23:51 -0400

    Added migrations

---
+++ Migrations/20230331183324_image-move.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PindexBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class imagemove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ItemId",
+                table: "Images");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ItemId",
+                table: "Images",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ItemId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ItemId",
+                table: "Images",
+                column: "ItemId",
+                unique: true);
+        }
+    }
+}
