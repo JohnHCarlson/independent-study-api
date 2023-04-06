aaa16fcacf6b8aa92b465cb0178e146450b7b213
John Carlson <jcjohncarlson@gmail.com>
2023-04-06 15:19:51 -0400

    Updated migrations

---
+++ Migrations/20230406182606_UpdateFileUpload.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PindexBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFileUpload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ItemId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemId",
+                table: "Images",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ItemId",
+                table: "Images",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "ItemId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
